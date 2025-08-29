b090b9e8cddf488b5562125892360802940bed79
keithli <keithlizimu@gmail.com>
2025-08-30 07:34:39 +0800

    Update weather_program.py

--- weather_program.py
+++ weather_program.py
@@ -1,8 +1,7 @@
 import requests
 import json
-province = input("输入省: ")
 url = "http://t.weather.itboy.net/api/weather/city/"
-with open("/Users/keithli/Downloads/Python/projects/china_weather_avg/weather_district_id.csv") as file:
+with open("weather_district_id.csv") as file:
     text = file.read()
 list_data = []
 lines = text.split("\n")
@@ -10,20 +9,26 @@ for i in lines:
     cols = i.split(",")
     list_data.append(cols)
 del list_data[0]
-stats = [[], [], [], [], [], [], []]
+stats = {}
 for i in list_data:
-    if i[10] == province:
-        try:
-            response = requests.get(url + i[0])
-            response = response.text
-            dict_data = json.loads(response)
-            data = dict_data["data"]["forecast"]
-            for j in range(0, 7):
-                temp = (int(data[j]["high"].replace("高温 ", "").replace("℃", "")) + int(data[j]["low"].replace("低温 ", "").replace("℃", "")))
-                temp /= 2
-                stats[j].append(temp)
-        except KeyError:
-            continue 
-for i in range(0, 7):
-    average = sum(stats[i]) / len(stats[i])
-    print(f"第{i + 1}天: {average:.2f}C")
+    province = i[10]
+    if province not in stats:
+        stats.update({province: {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: []}})
+    try:
+        response = requests.get(url + i[0])
+        response = response.text
+        dict_data = json.loads(response)
+        data = dict_data["data"]["forecast"]
+        for j in range(0, 7):
+            temp = int(data[j]["high"].replace("高温 ", "").replace("℃", ""))
+            temp += int(data[j]["low"].replace("低温 ", "").replace("℃", ""))
+            temp /= 2
+            stats[province][j + 1].append(temp)
+    except KeyError:
+        continue 
+for province, days in stats.items():
+    print(province)
+    for day, temp in days.items():
+        avg = sum(temp) / len(temp)
+        print(f"第{day}天: {avg:.2f}C")
+    print()
